8299b1008d26003884966eaf14dc16e44d46b12b
LayanNeuman <layanlord@gmail.com>
2024-12-13 07:13:33 +0530

    Add files via upload

---
+++ TaxiAva.php
@@ -0,0 +1,57 @@
+
+<?php require 'header.php';?>
+<main>
+    <div class="container-fluid">
+        <div class="row">
+        <div class="col">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title mb-4 d-inline">Taxi Avilabilty Details</h5>
+              <table class="table">
+                <thead class="thead-dark">
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Vehicle No</th>
+                    <th scope="col">Driver Name</th>
+                    <th scope="col">Distance Km</th>
+                    <th scope="col">Contact No</th>
+                    <th scope="col">Status</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>10</td>
+                    <td>12345</td>     
+                    <td>Busy</td> 
+                  </tr>
+
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>10</td>
+                    <td>12345</td>     
+                    <td>Busy</td> 
+                  </tr>
+
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>10</td>
+                    <td>12345</td>     
+                    <td>Busy</td> 
+                  </tr>
+
+                </tbody>
+              </table> 
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</main>
+<?php require 'footer.php';?>
